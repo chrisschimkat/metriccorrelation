6a9b02c52a012b0b039f0ea5fe9d9c911346c2f9
chrisschimkat <79619747+chrisschimkat@users.noreply.github.com>
2023-03-24 09:25:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.title("Metrics Correlations and Decay Effect")
+st.title("Metrics Correlations with Decay Effect")
 
 uploaded_file = st.file_uploader("Upload a CSV file:", type=['csv'])
 
@@ -18,24 +18,13 @@ if uploaded_file is not None:
             decayed_data.iloc[i] = decayed_data.iloc[i] + decayed_data.iloc[i - 1] * decay_rate
         return decayed_data
 
-    def find_time_to_reach_90_percent(decay_rate):
-        time_to_reach_90_percent = int(np.ceil(np.log(0.1) / np.log(decay_rate)))
-        return time_to_reach_90_percent
-
     decay_rate = 0.9
+    decayed_df = apply_decay(df, decay_rate)
+    decayed_correlations = decayed_df.corr()
 
-    for col in df.columns:
-        st.header(f"Results for {col}:")
+    st.header("Correlations with decay effect:")
 
-        correlations = df.corr()[[col]]
-        st.subheader("Correlations without decay effect:")
-        st.write(correlations)
-
-        decayed_df = apply_decay(df, decay_rate)
-        decayed_correlations = decayed_df.corr()[[col]]
-        st.subheader("Correlations with decay effect:")
-        st.write(decayed_correlations)
-
-        time_to_90_percent_effect = find_time_to_reach_90_percent(decay_rate)
-        st.subheader(f"Timeframe to reach 90% of the effect: {time_to_90_percent_effect} days")
-        st.write("=" * 50)
+    fig, ax = plt.subplots(figsize=(10, 8))
+    sns.heatmap(decayed_correlations, annot=True, fmt='.2f', cmap='coolwarm', vmin=-1, vmax=1, ax=ax)
+    ax.set_title('Correlations with decay effect')
+    st.pyplot(fig)
