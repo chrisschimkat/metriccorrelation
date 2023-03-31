e4a7c26f733749bc03e9f2dcfc1eb8585fe3596e
chrisschimkat <79619747+chrisschimkat@users.noreply.github.com>
2023-03-31 13:55:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import io
 from PIL import Image
 import requests
 from io import StringIO
-from streamlit.hashing import CodeHasher as _CodeHasher
+from st.hashing import CodeHasher as _CodeHasher
 
 class SessionState(object):
     def __init__(self, **kwargs):
